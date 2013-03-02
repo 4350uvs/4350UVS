c6935b48fd140a67bffb45e062997784037ce579
Umbruner <umbruner@cc.umanitoba.ca>
2013-03-02 13:39:37 -0600

    Add app_port_fix shell script, plus correct typo

--- scripts/app_port_fix.sh
+++ scripts/app_port_fix.sh
@@ -8,12 +8,24 @@
 #
 ########################################
 
+echo "Add an inbound rule TCP port 8274 to Security Group.";
+read -sp "Press [Enter] key to continue... "
+
 cd /
 cd /etc/nginx/sites-available/
 
 echo "Updating nginx config file.";
 
 echo "server {
+        listen 8274;
+
+        location / {
+               proxy_pass http://127.0.0.1:8001/4350api/;
+               proxy_set_header  X-Real-IP  $remote_addr;
+        }
+}
+
+server {
         listen 80;
 
         location / {
@@ -54,10 +66,19 @@ cd /home/www-data/web2py/
 
 echo "routes_out = (
   ('/4350app/(?P<any>.*)', '/\g<any>'),
+  ('/4350api/(?P<any>.*)', '/\g<any>'),
+)
+
+routes_in = (
+  ('/4350api/', '/4350api/default/index'),
+  ('/4350api/(?P<any>.*)', '/4350api/default/api/\g<any>'),
 )" > routes.py
 
-echo "Reloading nginx."
+echo "Reloading uwsgi."
+sudo /etc/init.d/uwsgi reload
 
+echo "Reloading nginx."
 sudo /etc/init.d/nginx reload
 
-echo "Finished web2py portion. Time to check if it works. If you see the app on your public dns address, it works.";
+echo "Finished the port fix. Time to check if it works. If you see the app on your public dns address, it works.";
+echo "If you see the api on [your dns]:8274, the api works.";
